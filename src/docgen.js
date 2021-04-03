79b483ce8a7bd5576dd4d1d86b020f90d5fb8cdd
Pau <frikeldon@gmail.com>
2021-04-03 21:14:26 +0200

    Se lee la documentacion de los componentes

--- src/docgen.js
+++ src/docgen.js
@@ -1,7 +1,28 @@
+const path = require('path')
+const fg = require('fast-glob')
+const { parse } = require('vue-docgen-api')
+
+async function readComponents ({ globComponents, cwd }) {
+  const componentPaths = await fg(globComponents, { cwd })
+  const components = []
+  for (const componentPath of componentPaths) {
+    const basename = path.basename(componentPath)
+    const fullComponentPath = path.join(cwd, componentPath)
+    const componentInfo = await parse(fullComponentPath)
+    components.push({
+      basename,
+      path: componentPath,
+      info: componentInfo
+    })
+  }
+  return components
+}
+
 module.exports = async function docgen ({
   cwd = process.cwd(),
   globComponents = '**/*.vue',
   outIndex = 'components.js',
   outComponents = './'
 } = {}) {
+  const components = await readComponents({ globComponents, cwd })
 }
